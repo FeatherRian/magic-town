84af7f1798f51ca751ac5df4017d166e9c629319
FeatherRian <1178441170@qq.com>
2022-08-02 21:37:48 +0800

    ✨ 添加游戏与生命组件的联动

--- assets/script/UI/Health.ts
+++ assets/script/UI/Health.ts
@@ -1,13 +1,21 @@
 import { _decorator, Component, Node, } from 'cc';
+import { Enemy } from '../Enemy/Enemy';
+import { RoundSystem } from '../GameManager/RoundSystem';
+import { Player } from '../Player/Player';
+import { DialogSystem } from './DialogSystem';
 const { ccclass, property } = _decorator;
 
 @ccclass('Health')
 export class Health extends Component {
     
-    private _currentHealth : number ;
+    @property(DialogSystem) dialogSystem : DialogSystem = null;
+    @property(RoundSystem) roundSystem : RoundSystem = null;
+    @property(Node) playerNode : Node = null;
+    @property(Node) enemyNode : Node = null;
+    private _currentHealth : number = 0;
     public maxHealth : number = 1000;
     public get currentHealth(){
-        return this._currentHealth
+        return this._currentHealth;
     }
     set currentHealth(value){
         this._currentHealth = value;
@@ -19,14 +27,29 @@ export class Health extends Component {
 
 
 
-    injured( damage:number){
-        if (this.currentHealth <= damage)
-        {
+    injured(damage:number){
+        if (this.currentHealth <= damage){
             this.currentHealth = 0;
+            if (this.node.name == "Player"){
+                //console.log(this);
+                this.dialogSystem.EnterDialog(0.05 , [this.playerNode.getComponent(Player).playerName+"受到"+this.enemyNode.getComponent(Enemy).enemyName+"的"+damage+"点伤害并被击败了"]);
+            }
+            if (this.node.name == "Enemy"){
+                //console.log(this);
+                this.dialogSystem.EnterDialog(0.05 , [this.playerNode.getComponent(Player).playerName+"对"+this.enemyNode.getComponent(Enemy).enemyName+"造成"+damage+"点伤害并击败了他"]);
+            }
             this.death();
         }
         else{
             this.currentHealth -= damage;
+            if (this.node.name == "Player"){
+                //console.log(this);
+                this.dialogSystem.EnterDialog(0.05 , [this.playerNode.getComponent(Player).playerName+"受到"+this.enemyNode.getComponent(Enemy).enemyName+"的"+damage+"点伤害"] , this.roundSystem.PlayerRoundBegin.bind(this.roundSystem));
+            }
+            if (this.node.name == "Enemy"){
+                //console.log(this);
+                this.dialogSystem.EnterDialog(0.05 , [this.playerNode.getComponent(Player).playerName+"对"+this.enemyNode.getComponent(Enemy).enemyName+"造成"+damage+"点伤害"] , this.roundSystem.EnemyRoundBegin.bind(this.roundSystem));
+            }
         }
     }
 
